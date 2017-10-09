5fe3197bde0fc7b5faed98c5cd071a0eb8453206
Sadi Mahmud <sadisagar@gmail.com>
2017-10-10 02:37:08 +0600

    Commit v 0.0.3

--- ShakeEventExpo.js
+++ ShakeEventExpo.js
@@ -12,27 +12,33 @@ import {
 } from 'expo';
 
 const THRESHOLD = 800;
+
 export class ShakeEventExpo {
   static addListener(handler) {
     let 
       last_x, 
       last_y,
       last_z;
-    let lastUpdate;
+    let lastUpdate = 0;
     Accelerometer.addListener(accelerometerData => {
         let {x, y, z} = accelerometerData;
-        let diffTime = (Date.now() - lastUpdate);
-        lastUpdate = Date.now();
-  
-        let speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-  
-        if ( speed > THRESHOLD ) {
-          console.log("sensor", "shake detected w/ speed: " + speed);
-          handler();
+        let currTime = Date.now();
+        if ((currTime - lastUpdate) > 100) {
+          let diffTime = (currTime - lastUpdate);
+          lastUpdate = currTime;
+    
+          let speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+    
+          if ( speed > THRESHOLD ) {
+            console.log(currTime + 'lastupdate: ' + lastUpdate);
+            console.log(diffTime);
+            console.log("sensor", "shake detected w/ speed: " + speed);
+            handler();
+          }
+          last_x = x;
+          last_y = y;
+          last_z = z;
         }
-        last_x = x;
-        last_y = y;
-        last_z = z;
     });
   }
   static removeListener() {
